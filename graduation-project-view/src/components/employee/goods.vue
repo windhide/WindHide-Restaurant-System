ad252d3cd3a5f6a64cb57b2a207fca39fc794a43
WindHide <WindHide520@gmail.com>
2023-02-13 23:23:27 +0800

    [Vue] [A] goods.vue新增上传功能，完善了逻辑编写

--- graduation-project-view/src/components/employee/goods.vue
+++ graduation-project-view/src/components/employee/goods.vue
@@ -27,7 +27,7 @@
             :total="totals" :current-page="nowPage" hide-on-single-page @current-change="handleSizeChange" />
     </el-scrollbar>
 
-    <el-dialog v-model="EDIT_DIALOG" title="菜品信息修改" width="30%" align-center>
+    <el-dialog v-model="EDIT_DIALOG" title="菜品信息修改" width="30%" align-center draggable :before-close="closeDialog">
         <el-form :model="goodsEditForm">
             <el-form-item label="菜品Id" :label-width="formLabelWidth">
                 <el-input v-model="goodsEditForm.goodsId" disabled />
@@ -36,7 +36,7 @@
                 <el-input v-model="goodsEditForm.goodsName" autocomplete="off" />
             </el-form-item>
             <el-form-item label="菜品图片" :label-width="formLabelWidth">
-                <el-upload accept="image/*" ref="AddItemLink" action="/api/upload/food" list-type="picture-card"
+                <el-upload accept="image/*" ref="EditUpload" action="/api/upload/food" list-type="picture-card"
                     :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :limit="2"
                     :on-success="ItemLinkSuccessAdd">
                     <el-icon class="avatar-uploader-icon">
@@ -60,22 +60,26 @@
                         :value=goodsType.goodsTypeId />
                 </el-select>
             </el-form-item>
+            <el-form-item label="现在的菜品图" :label-width="formLabelWidth">
+                <el-image style="width: 100px; height: 100px" :src="'/api/static/food/' + EditNowImage"
+                    fit="scale-down" />
+            </el-form-item>
         </el-form>
         <template #footer>
             <span class="dialog-footer">
-                <el-button @click="EDIT_DIALOG = false">取消</el-button>
+                <el-button @click="cancelDialog('EDIT')">取消</el-button>
                 <el-button type="primary" @click="CURRENCY_CRUD(URL, 'null', 2)">修改</el-button>
             </span>
         </template>
     </el-dialog>
 
-    <el-dialog v-model="ADD_DIALOG" title="菜品添加" width="30%" align-center>
+    <el-dialog v-model="ADD_DIALOG" title="菜品添加" width="30%" align-center draggable :before-close="closeDialog">
         <el-form :model="goodsAddForm">
             <el-form-item label="菜品名字" :label-width="formLabelWidth">
                 <el-input v-model="goodsAddForm.goodsName" autocomplete="off" />
             </el-form-item>
             <el-form-item label="菜品图片" :label-width="formLabelWidth">
-                <el-upload accept="image/*" ref="AddItemLink" action="/api/upload/food" list-type="picture-card"
+                <el-upload accept="image/*" ref="AddUpload" action="/api/upload/food" list-type="picture-card"
                     :on-preview="handlePictureCardPreview" :on-remove="handleRemove" :limit="2"
                     :on-success="ItemLinkSuccessAdd">
                     <el-icon class="avatar-uploader-icon">
@@ -90,9 +94,6 @@
             <el-form-item label="菜品折扣" :label-width="formLabelWidth">
                 <el-input v-model="goodsAddForm.goodsDiscount" autocomplete="off" />
             </el-form-item>
-            <el-form-item label="菜品价格" :label-width="formLabelWidth">
-                <el-input v-model="goodsAddForm.goodsPrice" autocomplete="off" />
-            </el-form-item>
             <el-form-item label="菜品类型" :label-width="formLabelWidth">
                 <el-select v-model="goodsAddForm.goodsTypeId" placeholder="选择菜品类型">
                     <el-option v-for="goodsType in goodsTypeList" :label=goodsType.goodsTypeName
@@ -102,13 +103,13 @@
         </el-form>
         <template #footer>
             <span class="dialog-footer">
-                <el-button @click="ADD_DIALOG = false">取消</el-button>
+                <el-button @click="cancelDialog('ADD')">取消</el-button>
                 <el-button type="primary" @click="CURRENCY_CRUD(URL, 'null', 1)">添加</el-button>
             </span>
         </template>
     </el-dialog>
     <el-dialog v-model="dialogindialogVisible" :append-to-body="true">
-         <img style="width:60%" :src="dialogImageUrl" alt="">
+        <img style="width:60%" :src="dialogImageUrl" alt="">
     </el-dialog>
 </template>
 
@@ -123,14 +124,19 @@ import {
     FORM_STATS_JUDGE
 } from "@/apis/normalCrudApi"
 import axios from 'axios';
+import { ElMessageBox, UploadInstance } from 'element-plus';
+
 let dialogImageUrl = ref('')
 let dialogindialogVisible = ref(false)
+let AddUpload = ref<UploadInstance>() // 对应上传的ref
+let EditUpload = ref<UploadInstance>()// 对应上传的ref
 
 let goodsList: any = reactive([])
 const URL = "goods" // 本组件内通用的url
 const formLabelWidth = '100px' // dialog中组件的宽度
 const EDIT_DIALOG = ref(false) // 修改dialog窗口开关
 const ADD_DIALOG = ref(false) // 添加dialog窗口开关
+let EditNowImage = ref("");
 let pageSize = ref(1); // 给初始值
 let totals = ref(1); // 给初始值
 let nowPage = ref(1);
@@ -190,6 +196,9 @@ async function CURRENCY_CRUD(url: String, data: any, operationId: Number) {
 
 async function DELETE(url: String, data: any, operationId: Number) {
     await CURRENCY_REQUEST(url, { removeId: data.goodsId }, CURRENCY_OPERATION_API(operationId, data.goodsName))
+    await axios.post("/uploadRemove/food/" + data.goodsImage)
+    goodsEditForm.goodsImage = ''
+    goodsAddForm.goodsImage = ''
     await RELOAD()
 }
 async function ADD(url: String, data: any, operationId: Number) {
@@ -198,6 +207,7 @@ async function ADD(url: String, data: any, operationId: Number) {
     if (!ADD_DIALOG.value && data == 'null' && FORM_STATS_JUDGE(goodsAddForm)) {
         await CURRENCY_REQUEST(url, goodsAddForm, CURRENCY_OPERATION_API(operationId, "->" + goodsAddForm.goodsName))
         goodsAddForm = CLEAR_FORM(goodsAddForm)
+        AddUpload.value!.clearFiles();
     }
     RELOAD()
 }
@@ -205,18 +215,48 @@ async function EDIT(url: String, data: any, operationId: Number) {
     if (!EDIT_DIALOG.value && data != 'null') {
         goodsEditForm.goodsId = data.goodsId
         goodsEditForm.goodsName = data.goodsName
-        goodsEditForm.goodsImage = data.goodsImage
+        // goodsEditForm.goodsImage = data.goodsImage
         goodsEditForm.goodsPrice = data.goodsPrice
         goodsEditForm.goodsDiscount = data.goodsDiscount
         goodsEditForm.goodsTypeId = data.goodsTypeId
         goodsEditForm.goodsType = data.goodsType
+        EditNowImage.value = data.goodsImage
         cacheData = data.goodsName
     }
     EDIT_DIALOG.value = !EDIT_DIALOG.value;
     if (!EDIT_DIALOG.value && data == 'null') {
-        await CURRENCY_REQUEST(url, goodsEditForm, CURRENCY_OPERATION_API(operationId, "前->" + cacheData + ",后->" + goodsEditForm.goodsName))
+        if(goodsEditForm.goodsImage == ""){
+            let cacheForm:any = reactive({})
+            cacheForm.goodsId = goodsEditForm.goodsId
+            cacheForm.goodsName = goodsEditForm.goodsName
+            cacheForm.goodsPrice = goodsEditForm.goodsPrice
+            cacheForm.goodsDiscount = goodsEditForm.goodsDiscount
+            cacheForm.goodsTypeId = goodsEditForm.goodsTypeId
+            cacheForm.goodsType = goodsEditForm.goodsType
+            await CURRENCY_REQUEST(url, cacheForm, CURRENCY_OPERATION_API(operationId, "前->" + cacheData + ",后->" + goodsEditForm.goodsName))
+        }else{
+            await CURRENCY_REQUEST(url, goodsEditForm, CURRENCY_OPERATION_API(operationId, "前->" + cacheData + ",后->" + goodsEditForm.goodsName))
+            await axios.post("/uploadRemove/food/" + EditNowImage.value)
+        }
+        EditUpload.value!.clearFiles();
         RELOAD()
         goodsEditForm = CLEAR_FORM(goodsEditForm)
+        EditNowImage.value = ""
+    }
+}
+
+function cancelDialog(addOrEdit: String) {
+    if (addOrEdit == "ADD") {
+        AddUpload.value!.clearFiles();
+        ADD_DIALOG.value = false
+        axios.post("/uploadRemove/food/" + goodsAddForm.goodsImage)
+        goodsAddForm = CLEAR_FORM(goodsAddForm)
+    } else {
+        EditUpload.value!.clearFiles();
+        EDIT_DIALOG.value = false
+        axios.post("/uploadRemove/food/" + goodsEditForm.goodsImage)
+        goodsEditForm = CLEAR_FORM(goodsEditForm)
+        EditNowImage.value = ""
     }
 }
 
@@ -234,19 +274,32 @@ function handleSizeChange(val: number) {
 
 function ItemLinkSuccessAdd(response: any, file: any, fileList: any) { //增加的上传图片方法
     if (fileList.length > 1) {
-        axios.post("/uploadRemove/food/"+fileList[0].response)
+        axios.post("/uploadRemove/food/" + fileList[0].response)
         fileList.splice(0, 1);
     }
     goodsEditForm.goodsImage = response
+    goodsAddForm.goodsImage = response
 }
 function handlePictureCardPreview(file: any) {
     dialogImageUrl = file.url;
     dialogindialogVisible.value = true;
 }
-function handleRemove(file: any) {
-    axios.post("/uploadRemove/food/"+goodsEditForm.goodsImage)
+function handleRemove() {
+    axios.post("/uploadRemove/food/" + goodsAddForm.goodsImage)
     goodsEditForm.goodsImage = ''
     goodsAddForm.goodsImage = ''
+}
+
+function closeDialog(){
+    ElMessageBox.confirm('确认关闭？').then(async (_) => {  
+        console.log(AddUpload.value)
+        axios.post("/uploadRemove/food/" + goodsAddForm.goodsImage)
+        axios.post("/uploadRemove/food/" + goodsEditForm.goodsImage)
+        EDIT_DIALOG.value = false
+        ADD_DIALOG.value = false
+        AddUpload.value!.clearFiles();
+        EditUpload.value!.clearFiles();
+      }).catch(_ => {});
 }
 
 RELOAD();
